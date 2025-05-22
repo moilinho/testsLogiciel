db679b81af492b9b4b8ec8948dc6b971a4c36371
YacineR7 <rachidi.yacine7@gmail.com>
2025-05-22 15:36:33 +0200

    création de test dans le fichier StatistiqueTests.java

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -6,12 +6,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.Mockito.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    @Test
+public void testCalculePrixMoyen() {
+
+    Voiture voiture1 = mock(Voiture.class);
+    Voiture voiture2 = mock(Voiture.class);
+
+
+    when(voiture1.getPrix()).thenReturn(50);
+    when(voiture2.getPrix()).thenReturn(1000);
+
+    StatistiqueImpl statistiqueImpl = new StatistiqueImpl();
+
+    statistiqueImpl.ajouter(voiture1);
+    statistiqueImpl.ajouter(voiture2);
+
+
+    Echantillon echantillon = statistiqueImpl.prixMoyen();
+
+
+    assertNotNull(echantillon); 
+    assertEquals(2, echantillon.getNombreDeVoitures());  
+    assertEquals(525, echantillon.getPrixMoyen()); 
+}
+    
 
 }
